2317ed57da36691e13f6a7656cfbeedd083c5dd6
Yuto0310 <yutoiwata1021@icloud.com>
2022-11-10 20:28:58 +0900

    Initial Commit

---
+++ CreateMandelbrotImages/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CreateMandelbrotImages
+//
+//  Created by 岩田悠杜 on 2022/11/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
